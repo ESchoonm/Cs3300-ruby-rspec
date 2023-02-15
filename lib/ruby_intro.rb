b18417a8e6f04bd1b64954d8778c3bf228d55ea9
evie_s <eschoonm@uccs.edu>
2023-02-15 17:53:18 +0000

    added code for part1, got all tests to pass, didn't explicitly change gemfile.lock, but I was messing around with extentions and such

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,14 +4,37 @@
 
 def sum arr
   # YOUR CODE HERE
+  arr.sum
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  if arr.length >= 2
+    sorted = arr.sort().reverse()
+    sum = sorted[0] + sorted[1]
+  else
+      sum = arr.sum
+  end
+return sum
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  if arr.length >= 2
+    length = arr.length
+    isSum = false
+  for i in 0..length-1 do
+    for j in i+1..length-1 do
+      if arr[i] + arr[j] == n
+        isSum = true
+        break
+      end
+    end
+  end
+  return isSum
+else
+  return false
+end
 end
 
 # Part 2
@@ -33,3 +56,4 @@ end
 class BookInStock
 # YOUR CODE HERE
 end
+puts "#{sum_to_n?([1,2,3,4,5], 7)}"
